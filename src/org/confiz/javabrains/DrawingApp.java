2e19039f800f7b86bbbed3201472a59dc7eeae80
aatif-kiani-confiz <aatif.kiani@confiz.com>
2016-08-26 14:45:30 +0500

    Spring Tutorial 04 - Writing Code Using the Bean Factory

--- src/org/confiz/javabrains/DrawingApp.java
+++ src/org/confiz/javabrains/DrawingApp.java
@@ -1,11 +1,7 @@
 package org.confiz.javabrains;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
