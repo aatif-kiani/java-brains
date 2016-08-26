ecbe164d8dac996d5297c78d887279b2b6a34027
aatif-kiani-confiz <aatif.kiani@confiz.com>
2016-08-26 14:55:42 +0500

    Spring Tutorial 05 - ApplicationContext and Property Initialization

--- src/org/confiz/javabrains/Triangle.java
+++ src/org/confiz/javabrains/Triangle.java
@@ -1,9 +1,18 @@
 package org.confiz.javabrains;
 
 public class Triangle {
+	
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 	public void draw() {
-		System.out.println("Triangle Drawn");
+		System.out.println(getType() + " Triangle Drawn");
 	}
-	
 }
